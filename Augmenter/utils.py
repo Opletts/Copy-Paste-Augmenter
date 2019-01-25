1601fe220d1bee34811c0e8e8171a283be12635f
opletts <apratim101@gmail.com>
2019-01-25 20:02:30 +0530

    Added CLAHE to histogram matching in utils

--- Augmenter/utils.py
+++ Augmenter/utils.py
@@ -31,13 +31,20 @@ def hist_match(source, template):
 
 	return interp_t_values[bin_idx].reshape(oldshape)
 
-def blend(roi, class_img):
+def blend(roi, class_img, hist_template, flag=1):
     gray = cv2.cvtColor(class_img, cv2.COLOR_BGR2GRAY)
     _, mask = cv2.threshold(gray, 5, 255, cv2.THRESH_BINARY)
     mask_inv = cv2.bitwise_not(mask)
 
-    class_img = smooth_edges(class_img, mask)
-    class_img = hist_match(class_img, roi)
+    if flag:
+        clahe = cv2.createCLAHE(clipLimit=4.0, tileGridSize=(8,8))
+        roi_lab = cv2.cvtColor(hist_template, cv2.COLOR_BGR2LAB)
+        roi_l, roi_a, roi_b = cv2.split(roi_lab)
+        roi_l = clahe.apply(roi_l)
+        roi_lab = cv2.merge((roi_l, roi_a, roi_b))
+        roi_cpy = cv2.cvtColor(roi_lab, cv2.COLOR_LAB2BGR)
+
+        class_img = hist_match(class_img, roi_cpy)
 
     bg = cv2.bitwise_and(roi, roi, mask=mask_inv)
     fg = cv2.bitwise_and(class_img, class_img, mask=mask)
@@ -47,10 +54,10 @@ def blend(roi, class_img):
 
 def smooth_edges(dst, mask):
     temp = dst.copy()
+    gray = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    _, mask = cv2.threshold(gray, 5, 255, cv2.THRESH_BINARY)
     edge = cv2.Canny(mask, 50, 150, 3)
     kernel = np.ones((5, 5), np.uint8)
     dilated = cv2.dilate(edge, kernel, iterations=1)
-    blurred = cv2.GaussianBlur(temp, (7, 7), 0)
+    blurred = cv2.GaussianBlur(temp, (5, 5), 0)
     dst[np.where(dilated != 0)] = blurred[np.where(dilated != 0)]
-
-    return dst
